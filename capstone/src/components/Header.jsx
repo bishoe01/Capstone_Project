33d049cd870fde68624c52a9aace9a042ad4e607
bishoe01 <bishoe01@gmail.com>
2023-01-16 15:47:44 +0900

    개발환경셋팅

---
+++ capstone/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header(props) {
+    return (
+        <div className='bg-black'>
+            hss
+        </div>
+    );
+}
+
+export default Header;
---
+++ capstone/src/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+import About from './pages/About';
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element : <App/>,
+    children:[
+      {index : true, path:'/' , element : <Home/>},
+      {path:'about' , element : <About/>},
+      {path:'contact' , element : <Contact/>},
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <RouterProvider router={router}/>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
