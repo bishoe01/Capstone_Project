254e1609ac942b21959a32f8d0678266ab492e7d
bishoe01 <bishoe01@gmail.com>
2023-01-24 00:50:48 +0900

    Arrayform사용 예약폼 생성

--- capstone/src/Reservepage/ReserveInput.jsx
+++ capstone/src/Reservepage/ReserveInput.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect,useState } from 'react';
 
-function ReserveInput({ options}) {
+function ReserveInput({options, type, placeholder}) {
     return (
-        <select onChange={(e) => console.log(e.target.value)} id="room"   className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-emphasize dark:placeholder-gray-400 dark:text-primary dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option defaultValue={"room"}>예약하실 팀플실을 선택하세요</option>
+        <select onChange={(e) => console.log(e.target.value)} id="room" className="bg-gray-50 border border-gray-300 text-gray-900 text-lg rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-white dark:border-emphasize dark:placeholder-gray-400 dark:text-primary dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option defaultValue={"room"}>{placeholder}</option>
             {options.map((option,index) => {
                 return (
-                    <option key={index}>{option}호</option>
+                    <option key={index}>{option}{type}</option>
                 );
             })}
         </select>
--- capstone/src/pages/TimeSelect.jsx
+++ capstone/src/pages/TimeSelect.jsx
@@ -15,7 +15,8 @@ function TimeSelect(props) {
     const { roomData } = useRoomContext();
     const [deparment, setDepartment] = useState(roomData[location.pathname.split('/')[2]]);
     const [onSection, setOnSection] = useState(false);
-    console.log(onSection);
+    const [y,m,d] = new Date().toLocaleDateString().split('.');
+    const today = m.concat('월 ', d, '일');
     return (
         <Fade>
             <StepBar step={2} />
@@ -28,17 +29,27 @@ function TimeSelect(props) {
                     {deparment.map((depart, index) => {
                         return (
                             <div key={index} className={`flex flex-col gap-1 p-4 items-start ${onSection[index] ? 'border-emphasize' : 'border-primary'} border-emphasize border-2 rounded-xl`}>
-                            <span className='text-xl border-sub border-b-2 p-1 drop-shadow-lg'>{depart}호</span>
+                                <span className='text-xl border-sub border-b-2 p-1 drop-shadow-lg'>{depart}호</span>
                                 <TimeTable />
                             </div>
                         )
                     })}
                 </div>
-                <div className='flex flex-col gap-6 p-4 absolute text-3xl -right-1/4 fixed w-2/6 h-auto rounded-lg border-primary border-2 shadow-md text-primary'>
+                <div className='flex flex-col gap-6 p-4 absolute text-3xl -right-1/4 fixed w-2/6 h-auto rounded-xl border-primary border-[3px] shadow-md shadow-sub text-primary'>
                     <h1 className='flex items-center gap-4'><MdOutlineEditCalendar />예약 정보</h1>
                     <form className='flex flex-col gap-4'>
                         <label htmlFor="room" className="block text-2xl text-primary font-medium text-gray-900 dark:text-primary">팀플실 번호</label>
-                        <ReserveInput options={deparment} />
+                        <ReserveInput options={deparment} type={'호'} placeholder={"예약하실 팀플실을 선택하세요"}/>
+                        <label htmlFor="room" className="block text-2xl text-primary font-medium text-gray-900 dark:text-primary">날짜</label>
+                        <ReserveInput options={Array.from({length:7} ,(v,index) => `${m}월 ${Number(d) + index}일`)} placeholder={"예약일을 선택해주세요"} />
+                        <label htmlFor="room" className="block text-2xl text-primary font-medium text-gray-900 dark:text-primary">이용시간</label>
+                        <div className='flex flex-col w-1/2 gap-2'>
+                        <ReserveInput options={time} type={':00'} placeholder='00 : 00' /> 
+                        <ReserveInput options={time} type={':00'} placeholder='00 : 00' /> 
+                        </div>
+                        <label htmlFor="room" className="block text-2xl text-primary font-medium text-gray-900 dark:text-primary">인원</label>
+                        <ReserveInput options={Array.from({length:5} ,(v,i) => `${i+2}인`)} placeholder={"인원 선택 (최소 2명이상)"} />
+                        <button className='mt-4 bg-primary text-white text-2xl rounded-lg p-2'>예약하기</button>
                     </form>
                 </div>
             </section>
