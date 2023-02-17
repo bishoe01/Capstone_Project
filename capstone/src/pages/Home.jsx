08ac82d664d7e9a4aad63fe1137b71f7ba5d69a6
bishoe01 <bishoe01@gmail.com>
2023-02-18 00:29:36 +0900

    recharts example 사용

--- capstone/src/pages/Home.jsx
+++ capstone/src/pages/Home.jsx
@@ -4,7 +4,60 @@ import '../App.css';
 import StepBar from '../components/StepBar';
 import Slide from 'react-reveal/Slide';
 import SectionCard from '../components/SectionCard';
-function Home({props}) {
+import {
+    LineChart,
+    Line,
+    XAxis,
+    YAxis,
+    CartesianGrid,
+    Tooltip,
+    Legend
+} from "recharts";
+const data = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100
+    }
+];
+function Home({ props }) {
     return (
         <>
             {/* <div className='flex flex-col justify-center items-center'>
@@ -13,7 +66,30 @@ function Home({props}) {
             </div> */}
 
             <section className='mt-10 flex flex-col w-full py-12 gap-10'>
-                
+                <LineChart
+                    width={800}
+                    height={400}
+                    data={data}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis dataKey="name" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Line
+                        type="monotone"
+                        dataKey="pv"
+                        stroke="#8884d8"
+                        activeDot={{ r: 8 }}
+                    />
+                    <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                </LineChart>
 
                 {/* <SectionCard text={'스터디/멘토링 등 소규모 모임'} direction='left'/>
                 <SectionCard text={'개강총회, 학부행사 등 대규모 행사'} direction='right'/> */}
