d22d050353388b431bcab11db89c5b6983f6e337
bishoe01 <bishoe01@gmail.com>
2023-03-24 11:03:22 +0900

    constans , styles , json 추가

--- capstone/src/components/Header.jsx
+++ capstone/src/components/Header.jsx
@@ -3,16 +3,16 @@ import { BsBook } from 'react-icons/bs';
 import { Link } from 'react-router-dom';
 function Header(props) {
 
-    const [links, setLinks] = useState(['Home', 'reserve', 'profile']);
+    const [links, setLinks] = useState(['Home','data','profile']);
     return (
         <header className='flex justify-between items-center p-6 border-b-2 mb-4 border-sub text-2xl text-primary'>
             <Link to={'/'} className='flex items-center' >
-                <BsBook />
-                <p className='ml-4 hover:text-emphasize'>HY-TOGETHER</p>
+            <BsBook/>
+            <p className='ml-4 hover:text-emphasize'>HY-TOGETHER</p>
             </Link>
             <ul className='flex gap-5 text-lg'>
-                {links.map((link, index) => {
-                    return (
+                {links.map((link,index) => {
+                return (
                         <li className=' hover:text-emphasize' key={index}>
                             <Link to={`/${link.toLowerCase()}`}>{link}</Link>
                         </li>
--- capstone/src/pages/ReservationDetail.jsx
+++ capstone/src/pages/ReservationDetail.jsx
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
 import Board from '../components/Board';
 import CurrentReservation from '../components/TimeTable/CurrentReservation';
 import Fade from 'react-reveal/Fade';
 import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 function ReservationDetail(props) {
     const hours = [9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
